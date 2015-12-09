e6768dc7d24b6182fdb6459a61d4ceaa5864a819
isgustavo <himynameisgustavo@gmail.com>
2015-12-09 14:46:40 -0200

    Initial Commit

---
+++ Traveling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Traveling
+//
+//  Created by gustavo.freitas2 on 12/9/15.
+//  Copyright © 2015 Pé de pano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
